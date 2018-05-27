0e34c063ea65845c2062cb9cfce08f937880f1d6
Unknown <chatajazzy@interia.pl>
2018-05-27 21:43:14 +0200

    Moved title to top in single post view and aligned left

--- wp-content/themes/grafique/template-parts/content-single.php
+++ wp-content/themes/grafique/template-parts/content-single.php
@@ -23,11 +23,7 @@ if ('post' === get_post_type()):
 
 			<div class="entry-meta">
 				<header class="entry-header entry-meta">
-                <figure class="post-thumbnail">
-                            <a href="<?=esc_url(get_permalink())?>" class="post-thumbnail__link">
-                                <img class="post-thumbnail__img" src="/wp-content/themes/grafique/img/image1.jpg" alt="cos">
-                            </a>
-                        </figure>
+          
 
                                 <span class="post__date">
 																<a href="<?=get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));?>" class="post__date-link"><?php the_time('F j, Y')?></a>
@@ -39,6 +35,7 @@ if ('post' === get_post_type()):
                                     <a href="" rel="tag"><?=the_category(', ');?></a>
                                 </span>
                             </header>
+                            
 
 			</div><!-- .entry-meta -->
 		<?php endif;?>
@@ -47,7 +44,11 @@ if ('post' === get_post_type()):
 
 <div class="entry-content">
 	<?=the_title('<h3 class="entry-title">', '</h3>')?>
-
+    <figure class="post-thumbnail">
+                            <a href="<?=esc_url(get_permalink())?>" class="post-thumbnail__link">
+                                <img class="post-thumbnail__img" src="/wp-content/themes/grafique/img/image1.jpg" alt="cos">
+                            </a>
+                        </figure>
 <?=the_content()?>
                             </div>
 
